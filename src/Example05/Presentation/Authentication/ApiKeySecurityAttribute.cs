8102e5f855033a9bf0acde0036b12a8fd58bf5e9
Aymen TROUDI <Aymen.TROUDI@sodexo.com>
2023-05-11 22:55:20 +0200

    Fix security attribute

--- src/Example05/Presentation/Authentication/ApiKeySecurityAttribute.cs
+++ src/Example05/Presentation/Authentication/ApiKeySecurityAttribute.cs
@@ -1,24 +1,11 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Filters;
 
 namespace Example05.Presentation.Authentication;
 
 [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
-public class ApiKeySecurityAttribute : Attribute, IAuthorizationFilter
+public class ApiKeySecurityAttribute : TypeFilterAttribute
 {
-    public void OnAuthorization(AuthorizationFilterContext context)
+    public ApiKeySecurityAttribute() : base(typeof(ApiKeySecurityFilter))
     {
-        if (!context.HttpContext.Request.Headers.TryGetValue(ApiKeyConstants.ApiKeyHeaderName, out var requestApiKey))
-        {
-            context.Result = new UnauthorizedObjectResult("Api key is missing");
-            return;
-        }
-
-        var configuration = context.HttpContext.RequestServices.GetRequiredService<IConfiguration>();
-        var actualApiKey = configuration.GetValue<string>(ApiKeyConstants.ApiKeySectionName);
-        if (requestApiKey != actualApiKey)
-        {
-            context.Result = new UnauthorizedObjectResult("Api key is invalid");
-        }
     }
 }
